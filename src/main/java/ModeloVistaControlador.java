5e60d9c9568bc5a662986b56d091edd78ccfd3ef
anibal0531 <anibal.toro@utp.edu.co>
2019-06-09 16:54:07 -0500

    modelo vista controlador

--- src/main/java/ModeloVistaControlador.java
+++ src/main/java/ModeloVistaControlador.java
@@ -4,6 +4,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class ModeloVistaControlador extends JFrame {
 
@@ -33,8 +35,23 @@ public class ModeloVistaControlador extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel label = new JLabel("");
+		label.setBounds(161, 139, 46, 14);
+		contentPane.add(label);
+		
+		JButton btnSoliciteTurno = new JButton("SOLICITE TURNO");
+		btnSoliciteTurno.setBounds(139, 35, 160, 23);
+		contentPane.add(btnSoliciteTurno);
+		
+		JButton btnListaEnEspera = new JButton("LISTA EN ESPERA");
+		btnListaEnEspera.setBounds(139, 105, 160, 23);
+		contentPane.add(btnListaEnEspera);
+		
+		JButton btnEstadoAsesor = new JButton("ESTADO ASESOR");
+		btnEstadoAsesor.setBounds(139, 181, 160, 23);
+		contentPane.add(btnEstadoAsesor);
 	}
-
 }
